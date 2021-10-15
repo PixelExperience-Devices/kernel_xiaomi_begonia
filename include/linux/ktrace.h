d51a11bc48b4f6dbe81fbfa608905c6114158c19
TheMalachite <eliasgheeraert@gmail.com>
2021-10-16 00:00:09 +0200

    drivers: staging: Drop weird MIUI ktrace and rtmm symlinks logic
    This just generate useless symlinked files who can randomly break kernel building
    
    Change-Id: I7e40590edbfc2070b62fb3f577da80ec2cf13008

--- include/linux/ktrace.h
+++ include/linux/ktrace.h
@@ -1,1 +1,4 @@
-/home/elias/github/mediatek/mt6785/xiaomi/begonia/lineage/android_kernel_xiaomi_mt6785/include/dum/ktrace.h
+#ifndef __KTRACE_H__
+#define __KTRACE_H__
+
+#endif
--- include/linux/rtmm.h
+++ include/linux/rtmm.h
@@ -1,1 +1,26 @@
-/home/elias/github/mediatek/mt6785/xiaomi/begonia/lineage/android_kernel_xiaomi_mt6785/include/dum/rtmm.h
+#ifndef __RTMM_H__
+#define __RTMM_H__
+
+enum {
+	RTMM_POOL_THREADINFO = 0,
+	RTMM_POOL_PGD,
+	RTMM_POOL_KMALLOC_ORDER2,
+	RTMM_POOL_NR
+};
+
+#define KMALLOC_POOL_ORDER2 2
+static inline bool rtmm_pool(const char *name)
+{
+	return false;
+}
+
+static inline bool rtmm_reclaim(const char *name)
+{
+	return false;
+}
+
+static inline int rtmm_reclaim_swappiness(void)
+{
+	return 60;
+}
+#endif
